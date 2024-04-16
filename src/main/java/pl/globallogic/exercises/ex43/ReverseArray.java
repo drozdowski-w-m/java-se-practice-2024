33fcfcd2813c79a9e1502db1fe361b7ca931b206
wojciech.drozdowski <drozdowski.w.m@gmail.com>
2024-04-16 13:49:47 +0200

    update

--- src/main/java/pl/globallogic/exercises/ex43/ReverseArray.java
+++ src/main/java/pl/globallogic/exercises/ex43/ReverseArray.java
@@ -1,13 +1,32 @@
 package pl.globallogic.exercises.ex43;
 
+import java.util.Arrays;
+
 public class ReverseArray {
     public static void reverse(int[] array){
 
+        System.out.println("Input array: "+Arrays.toString(array));
+
         int lastElementIndex = array.length-1;
+        int firstElementIndex = 0;
         int temp = 0;
 
-        for (int i = 0; i < array.length; i++) {
+        while (lastElementIndex > firstElementIndex){
+            temp = array[firstElementIndex];
+            array[firstElementIndex] = array[lastElementIndex];
+            array[lastElementIndex] = temp;
 
+            firstElementIndex++;
+            lastElementIndex--;
         }
+
+        System.out.println("Reversed array: "+Arrays.toString(array));
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6};
+        reverse(array);
+
+
     }
 }
